abd0591ee3cae5cc4d54c62afc088862bcc44638
Farshad Hasanpour <farshad.hasanpour96@gmail.com>
2023-06-25 19:45:18 +0330

    added ripple

--- components/PageHeader.vue
+++ components/PageHeader.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="top-bar">
 		<h1 class="top-bar__title">Where in the world?</h1>
-		<button class="top-bar__theme" @click="$store.commit('toggleDarkMode')">
+		<button v-wave class="top-bar__theme" @click="$store.commit('toggleDarkMode')">
 			<CIcon :name="$store.state.isDark ? $store.state.icons.lightMode : $store.state.icons.darkMode"/>
 			<span>{{ $store.state.isDark ? 'Light Mode' : 'Dark Mode' }}</span>
 		</button>
@@ -19,6 +19,6 @@ export default {
 		display: flex;
 		align-items: center;
 		justify-content: space-between;
-
+		height: 64px;
 	}
 </style>
