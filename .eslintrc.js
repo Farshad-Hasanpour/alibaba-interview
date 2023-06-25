969920f79e0654eca18f493fddc9d71cd09805ea
Farshad Hasanpour <farshad.hasanpour96@gmail.com>
2023-06-25 15:59:04 +0330

    updated config files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,36 @@
 module.exports = {
-  root: true,
-  env: {
-    browser: true,
-    node: true
-  },
-  parserOptions: {
-    parser: '@babel/eslint-parser',
-    requireConfigFile: false
-  },
-  extends: [
-    '@nuxtjs',
-    'plugin:nuxt/recommended'
-  ],
-  plugins: [
-  ],
-  // add your custom rules here
-  rules: {}
+	root: true,
+	env: {
+		browser: true,
+		node: true
+	},
+	parserOptions: {
+		parser: '@babel/eslint-parser',
+		requireConfigFile: false
+	},
+	extends: [
+		'@nuxtjs',
+		'plugin:nuxt/recommended'
+	],
+	plugins: [],
+	// add your custom rules here
+	rules: {
+		"no-tabs": ["error", {"allowIndentationTabs": true}],
+		"space-before-function-paren": "off",
+		"indent": "off",
+		"space-before-blocks": "off",
+		"semi": "off",
+		"key-spacing": "off",
+		"keyword-spacing": "off",
+		"comma-dangle": "off",
+		"no-tabs": "off",
+		"curly": "off",
+		"spaced-comment": "off",
+		"curly": "off",
+		"quotes": "off",
+		"arrow-parens": "off",
+		"vue/html-indent": "off",
+		"vue/singleline-html-element-content-newline": "off",
+		"object-shorthand": "off"
+	}
 }
