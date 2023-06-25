322c5c574cb535c0d6a72c90ab4a5f7d175eb223
Farshad Hasanpour <farshad.hasanpour96@gmail.com>
2023-06-25 19:30:51 +0330

    added font

--- nuxt.config.js
+++ nuxt.config.js
@@ -32,8 +32,18 @@ export default {
 	// Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
 	buildModules: [
 		// https://go.nuxtjs.dev/eslint
-		'@nuxtjs/eslint-module'
+		'@nuxtjs/eslint-module',
+		'@nuxtjs/google-fonts'
 	],
+
+	googleFonts: {
+		families: {
+			'Nunito Sans': {
+				wght: [300, 600, 800],
+			}
+		},
+		display: 'swap'
+	},
 
 	// Modules: https://go.nuxtjs.dev/config-modules
 	modules: [
